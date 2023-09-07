24f3880d7c12d4199971d772fe862bd70cbff219
Nickyi175 <124185028+Nickyi175@users.noreply.github.com>
2023-09-07 12:09:17 -0600

    playercomprobante

--- src/LOGIN/Player_Comprobante1.java
+++ src/LOGIN/Player_Comprobante1.java
@@ -4,6 +4,10 @@
  */
 package LOGIN;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
 /**
  *
  * @author MARTI
@@ -41,5 +45,15 @@ public class Player_Comprobante1 {
     public static Creacion_Player1 obtener(String username) {
         return userIn.obtener(username);
     }
-
+    private static void guardarUsuarios() {
+        try {
+            FileOutputStream fileOut = new FileOutputStream("usuarios.ser");
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(userIn.getUsers());
+            out.close();
+            fileOut.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
